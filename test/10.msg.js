6152226fdf613987894ad7a5b0150a3a209c36da
Yusuke Kawasaki <u-suke@kawa.net>
2017-12-13 18:58:40 +0900

    npm run test

--- test/10.msg.js
+++ test/10.msg.js
@@ -1,5 +1,5 @@
 "use strict";
-exports.__esModule = true;
+Object.defineProperty(exports, "__esModule", { value: true });
 var assert = require("assert");
 var _1 = require("../");
 var TITLE = __filename.split("/").pop();
