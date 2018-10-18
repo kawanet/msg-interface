8d025bb8d620c7fdaaca210a03e159c476e4b206
Yusuke Kawasaki <u-suke@kawa.net>
2018-10-18 16:23:09 +0900

    TypeScript dependency removed

--- test/10.msg.js
+++ test/10.msg.js
@@ -1,96 +1,90 @@
 "use strict";
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-        return extendStatics(d, b);
+
+const assert = require("assert");
+const MI = require("..");
+const Msg = MI.Msg;
+const isMsg = MI.isMsg;
+const msgToBuffer = MI.msgToBuffer;
+
+const TITLE = __filename.split("/").pop();
+
+const atos = array => [].map.call(array, v => (v > 15 ? "" : "0") + v.toString(16)).join("-");
+
+describe(TITLE, () => {
+  it("Msg", () => {
+    class MsgTest extends Msg {
+      //
     }
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-Object.defineProperty(exports, "__esModule", { value: true });
-var assert = require("assert");
-var __1 = require("../");
-var TITLE = __filename.split("/").pop();
-var atos = function (array) { return [].map.call(array, function (v) { return (v > 15 ? "" : "0") + v.toString(16); }).join("-"); };
-describe(TITLE, function () {
-    it("Msg", function () {
-        var MsgTest = /** @class */ (function (_super) {
-            __extends(MsgTest, _super);
-            function MsgTest() {
-                return _super !== null && _super.apply(this, arguments) || this;
-            }
-            return MsgTest;
-        }(__1.Msg));
-        var msg = new MsgTest();
-        assert.strictEqual(typeof __1.isMsg, "function");
-        assert.strictEqual(typeof msg.writeMsgpackTo, "function");
-        assert.strictEqual(typeof __1.msgToBuffer, "function");
-        assert(!__1.isMsg(null));
-        assert(!__1.isMsg(0));
-        assert(!__1.isMsg(1));
-        assert(!__1.isMsg({}));
-        // Error: Invalid msgpackLength
-        assert.throws(function () { return __1.msgToBuffer(msg); });
-        // Error: Method not implemented: writeMsgpackTo
-        assert.throws(function () { return msg.writeMsgpackTo(Buffer.alloc(2)); });
-    });
-    it("writeMsgpackTo", function () {
-        var MsgTest = /** @class */ (function (_super) {
-            __extends(MsgTest, _super);
-            function MsgTest() {
-                return _super !== null && _super.apply(this, arguments) || this;
-            }
-            MsgTest.prototype.writeMsgpackTo = function (buffer, offset) {
-                offset = 0 | offset;
-                return buffer.writeUInt16BE(0x0708, offset);
-            };
-            return MsgTest;
-        }(__1.Msg));
-        var msg = new MsgTest();
-        // Error: Invalid msgpackLength
-        assert.strictEqual(msg.msgpackLength, void 0);
-        assert.throws(function () { return __1.msgToBuffer(msg); });
-        // msgToBuffer
-        msg.msgpackLength = 2;
-        assert(__1.isMsg(msg));
-        assert.strictEqual(atos(__1.msgToBuffer(msg)), "07-08");
-        // writeMsgpackTo with offset
-        var buf = Buffer.from([9, 10, 11, 12]);
-        msg.writeMsgpackTo(buf, 1);
-        assert.strictEqual(atos(buf), "09-07-08-0c");
-        // writeMsgpackTo without offset
-        msg.writeMsgpackTo(buf);
-        assert.strictEqual(atos(buf), "07-08-08-0c");
-    });
-    it("MsgString", function () {
-        var MsgString32 = /** @class */ (function (_super) {
-            __extends(MsgString32, _super);
-            function MsgString32() {
-                return _super !== null && _super.apply(this, arguments) || this;
-            }
-            MsgString32.from = function (string) {
-                var msg = new MsgString32();
-                msg.value = string;
-                // maximum byte length
-                msg.msgpackLength = 5 + string.length * 3;
-                return msg;
-            };
-            MsgString32.prototype.writeMsgpackTo = function (buffer, offset) {
-                offset = 0 | offset;
-                buffer[offset] = 0xdb;
-                // actual byte length
-                var length = buffer.write(this.value, offset + 5);
-                buffer.writeUInt32BE(length, offset + 1);
-                return 5 + length;
-            };
-            return MsgString32;
-        }(__1.Msg));
-        var msg = MsgString32.from("ABC");
-        assert.strictEqual(atos(__1.msgToBuffer(msg)), "db-00-00-00-03-41-42-43");
-    });
+
+    const msg = new MsgTest();
+
+    assert.strictEqual(typeof isMsg, "function");
+    assert.strictEqual(typeof msg.writeMsgpackTo, "function");
+    assert.strictEqual(typeof msgToBuffer, "function");
+
+    assert(!isMsg(null));
+    assert(!isMsg(0));
+    assert(!isMsg(1));
+    assert(!isMsg({}));
+
+    // Error: Invalid msgpackLength
+    assert.throws(() => msgToBuffer(msg));
+
+    // Error: Method not implemented: writeMsgpackTo
+    assert.throws(() => msg.writeMsgpackTo(Buffer.alloc(2)));
+  });
+
+  it("writeMsgpackTo", () => {
+    class MsgTest extends Msg {
+      writeMsgpackTo(buffer, offset) {
+        offset |= 0;
+        return buffer.writeUInt16BE(0x0708, offset);
+      }
+    }
+
+    const msg = new MsgTest();
+
+    // Error: Invalid msgpackLength
+    assert.strictEqual(msg.msgpackLength, void 0);
+    assert.throws(() => msgToBuffer(msg));
+
+    // msgToBuffer
+    msg.msgpackLength = 2;
+    assert(isMsg(msg));
+    assert.strictEqual(atos(msgToBuffer(msg)), "07-08");
+
+    // writeMsgpackTo with offset
+    const buf = Buffer.from([9, 10, 11, 12]);
+    msg.writeMsgpackTo(buf, 1);
+    assert.strictEqual(atos(buf), "09-07-08-0c");
+
+    // writeMsgpackTo without offset
+    msg.writeMsgpackTo(buf);
+    assert.strictEqual(atos(buf), "07-08-08-0c");
+  });
+
+  it("MsgString", () => {
+    class MsgString32 extends Msg {
+      static from(string) {
+        const msg = new MsgString32();
+        msg.value = string;
+        // maximum byte length
+        msg.msgpackLength = 5 + string.length * 3;
+        return msg;
+      }
+
+      writeMsgpackTo(buffer, offset) {
+        offset = 0 | offset;
+        buffer[offset] = 0xdb;
+        // actual byte length
+        const length = buffer.write(this.value, offset + 5);
+        buffer.writeUInt32BE(length, offset + 1);
+
+        return 5 + length;
+      }
+    }
+
+    const msg = MsgString32.from("ABC");
+    assert.strictEqual(atos(msgToBuffer(msg)), "db-00-00-00-03-41-42-43");
+  });
 });
